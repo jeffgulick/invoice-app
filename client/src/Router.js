4fb1ebff223c184ca287beedc97e31d1f5a95830
JEFFGULICK <jeffgulick@gmail.com>
2021-06-08 20:58:33 -0500

    working on invoice style

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import Main from './Components/Main'
-import Invoice from './Components/Invoice'
-import {Switch, Route, Redirect} from 'react-router'
+import React from 'react';
+import Main from './Components/Main';
+import Invoice from './Components/Invoice/Invoice';
+import { Switch, Route, Redirect } from 'react-router';
 
 const Router = () => {
-    return (
-        <Switch>
-            <Route exact path='/' component={Main} />
-            <Route path='/invoice' component={Invoice} />
-        </Switch>
-    );
-}
- 
+  return (
+    <Switch>
+      <Route exact path='/' component={Main} />
+      <Route path='/invoice' component={Invoice} />
+    </Switch>
+  );
+};
+
 export default Router;
